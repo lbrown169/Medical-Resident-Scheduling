f9505e76b63f69d07389c06969977dbe1f34ca9b
Abhinav-Pollepalli <pollepallia@gmail.com>
2025-10-21 21:14:53 -0400

    fixed .editorconfig formatting issues
    fixed .editorconfig formatting issues

--- backend/Controllers/ResidentsController.cs
+++ backend/Controllers/ResidentsController.cs
@@ -11,7 +11,7 @@ namespace MedicalDemo.Controllers;
 public class ResidentsController : ControllerBase
 {
     private readonly MedicalContext _context;
-    
+
 
 
     public ResidentsController(MedicalContext context)
--- backend/Program.cs
+++ backend/Program.cs
@@ -54,7 +54,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
