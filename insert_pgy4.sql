a7f2ff49f0ad8732f4b7847339dbaeac99fabb68
Dylan Money <money.dylan8902@gmail.com>
2026-02-09 09:16:53 -0500

    Delete insert_pgy4.sql

--- insert_pgy4.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO residents (resident_id, email, password, first_name, last_name, graduate_yr, phone_num, weekly_hours, total_hours, bi_yearly_hours)
-VALUES ('PGY4TST', 'pgy4@demo.com', '$2b$12$KqebvwNbxyuEg1OIOSjYZuM8gHK6V0P0yQ0Z5X0Z5X0Z5X0Z5X0Z5O', 'Sarah', 'Johnson', 2023, '4444444444', 0, 0, 0);
