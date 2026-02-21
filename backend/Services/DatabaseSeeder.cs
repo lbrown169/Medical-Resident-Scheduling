934ddab9ccafaf65a5361674be8ce717a279cd41
Xutao Gao <xu611268@ucf.edu>
2026-02-21 16:05:34 -0500

    G23-117: Remove redundant PGY3 to only 8 PGY3s and designated 4 of the PGY3 as Chief resident

--- backend/Services/DatabaseSeeder.cs
+++ backend/Services/DatabaseSeeder.cs
@@ -113,21 +113,6 @@ public class DatabaseSeeder
             },
             new Resident
             {
-                ResidentId = "EIC4231",
-                FirstName = "Natalie",
-                LastName = "Portman",
-                GraduateYr = 3,
-                Email = "natalie.portman@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0108",
-                WeeklyHours = 0,
-                TotalHours = 9,
-                BiYearlyHours = 9,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
                 ResidentId = "FEU3416",
                 FirstName = "Robert",
                 LastName = "Downey",
@@ -173,21 +158,6 @@ public class DatabaseSeeder
             },
             new Resident
             {
-                ResidentId = "GEV4598",
-                FirstName = "Emma",
-                LastName = "Stone",
-                GraduateYr = 3,
-                Email = "emma.stone@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0112",
-                WeeklyHours = 0,
-                TotalHours = 9,
-                BiYearlyHours = 9,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
                 ResidentId = "GKU3319",
                 FirstName = "Ryan",
                 LastName = "Gosling",
@@ -214,7 +184,8 @@ public class DatabaseSeeder
                 WeeklyHours = 0,
                 TotalHours = 9,
                 BiYearlyHours = 9,
-                HospitalRoleProfile = null
+                HospitalRoleProfile = null,
+                ChiefType = Enums.ChiefType.Clinic
             },
             new Resident
             {
@@ -394,7 +365,8 @@ public class DatabaseSeeder
                 WeeklyHours = 0,
                 TotalHours = 9,
                 BiYearlyHours = 9,
-                HospitalRoleProfile = null
+                HospitalRoleProfile = null,
+                ChiefType = Enums.ChiefType.Admin
             },
             new Resident
             {
@@ -428,111 +400,6 @@ public class DatabaseSeeder
             },
             new Resident
             {
-                ResidentId = "OFS2842",
-                FirstName = "Dwayne",
-                LastName = "Johnson",
-                GraduateYr = 3,
-                Email = "dwayne.johnson@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 43,
-                BiYearlyHours = 23,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "LKD1234",
-                FirstName = "Tom",
-                LastName = "Cruise",
-                GraduateYr = 3,
-                Email = "tom.cruise@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 9,
-                BiYearlyHours = 9,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "AKC1234",
-                FirstName = "Justin",
-                LastName = "Bieber",
-                GraduateYr = 3,
-                Email = "justin.bieber@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 17,
-                BiYearlyHours = 14,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "POW8237",
-                FirstName = "Tom",
-                LastName = "Holland",
-                GraduateYr = 3,
-                Email = "tom.holland@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 78,
-                BiYearlyHours = 51,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "KJD8273",
-                FirstName = "Pedro",
-                LastName = "Pascal",
-                GraduateYr = 3,
-                Email = "pedro.pascal@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 41,
-                BiYearlyHours = 39,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "MWU2643",
-                FirstName = "Margot",
-                LastName = "Robbie",
-                GraduateYr = 3,
-                Email = "margot.robbie@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 10,
-                BiYearlyHours = 4,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "KSK8232",
-                FirstName = "Chris",
-                LastName = "Rock",
-                GraduateYr = 3,
-                Email = "chris.rock@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 23,
-                BiYearlyHours = 18,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
                 ResidentId = "VKD7295",
                 FirstName = "Jackie",
                 LastName = "Chan",
@@ -544,7 +411,8 @@ public class DatabaseSeeder
                 WeeklyHours = 0,
                 TotalHours = 87,
                 BiYearlyHours = 67,
-                HospitalRoleProfile = null
+                HospitalRoleProfile = null,
+                ChiefType = Enums.ChiefType.Education
             },
             new Resident
             {
@@ -559,22 +427,8 @@ public class DatabaseSeeder
                 WeeklyHours = 0,
                 TotalHours = 11,
                 BiYearlyHours = 4,
-                HospitalRoleProfile = null
-            },
-            new Resident
-            {
-                ResidentId = "ZJT8738",
-                FirstName = "Jason",
-                LastName = "Momoa",
-                GraduateYr = 3,
-                Email = "json.momoa@example.com",
-                Password
-                    = "$2b$12$KqebvwNbxyuEg1OIOSjYZuMdXEDtEcbnsYwB9MHmSQgVsr.kHPWa.",
-                PhoneNum = "201-555-0128",
-                WeeklyHours = 0,
-                TotalHours = 20,
-                BiYearlyHours = 12,
-                HospitalRoleProfile = null
+                HospitalRoleProfile = null,
+                ChiefType = Enums.ChiefType.Clinic
             }
         ];
 
