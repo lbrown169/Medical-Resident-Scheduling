0428c67303bfdb51673de60074019f43c9623864
Sam41704 <sam41704@gmail.com>
2026-03-06 17:19:13 -0500

    Added page and hooked up to nav

---
+++ frontend/src/app/dashboard/components/PGY12RotationPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PGY12RotationPage() {
+  return (
+    <div className="w-full pt-4 flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-6">PGY1 & PGY2 Rotations</h1>
+    </div>
+  );
+}
